3ffbc236bc8d0ba0563272140104d6282f2c30b2
Loïc Baccigalupi <loic.baccigalupi@gmail.com>
2022-09-01 02:08:17 +0200

    Started base

--- study-notes-browser/src/App.vue
+++ study-notes-browser/src/App.vue
@@ -1,85 +1,77 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+import type { GHContents } from "@/types";
+
+export default defineComponent({
+  data() {
+    return {
+      contents: [] as GHContents[],
+      basePath: "",
+    };
+  },
+  methods: {
+    async fetch() {
+      const res = await axios({
+        method: "get",
+        url: `https://api.github.com/repos/loicbacciga/study-notes/contents/${this.basePath}`,
+        headers: {
+          Accept: "application/vnd.github+json",
+          Authorization: `Bearer ${import.meta.env.VITE_GH_TOKEN}`,
+        },
+      });
+
+      console.log(res.data);
+      this.contents = res.data as GHContents[];
+    },
+
+    goTo(index: number) {
+      this.basePath = this.basePath
+        .split("/")
+        .slice(0, index + 1)
+        .join("/");
+    },
+  },
+  watch: {
+    basePath() {
+      this.fetch();
+    },
+  },
+
+  created() {
+    this.fetch();
+  },
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <p>Path: "{{ basePath }}"</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <p>
+      <a @click="basePath = ''">Home</a>
+      <a
+        v-show="basePath.length > 0"
+        v-for="[i, path] in basePath.split('/').entries()"
+        :key="path"
+        @click="goTo(i)"
+      >
+        -
+        {{ path }}
+      </a>
+    </p>
+    <ul>
+      <li v-for="content in contents" :key="content.name">
+        <button @click="basePath = content.path">
+          {{ content.name }} | {{ content.type }}
+          <iframe
+            v-if="content.type === 'file'"
+            :src="`https://docs.google.com/gview?url=${content.download_url}f&embedded=true`"
+            style="width: 600px; height: 500px"
+            frameborder="0"
+          ></iframe>
+        </button>
+      </li>
+    </ul>
+  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ study-notes-browser/src/types.d.ts
@@ -0,0 +1,6 @@
+export interface GHContents {
+  name: string;
+  path: string;
+  type: string;
+  download_url: string;
+}
